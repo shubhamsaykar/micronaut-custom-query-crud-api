776bb7239b61d8ccdf730eee15a635ed40b15747
pranav9798; <chavan.pranav9798@gmail.com;>
2022-06-23 15:52:51 +0530

    initial commit

--- src/main/java/com/anemoi/Application.java
+++ src/main/java/com/anemoi/Application.java
@@ -7,6 +7,7 @@ public class Application {
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
 		System.out.println("connection istablish");
+		System.out.println("hi i am shubham");
 
     }
 }
